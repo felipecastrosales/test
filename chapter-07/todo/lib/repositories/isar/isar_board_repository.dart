ce510f95ce26e38180095e54bad575c99e39a27c
Felipe Sales <soufeliposales@gmail.com>
2023-09-24 20:24:13 -0300

    feat: implements IsarBoardRepository and add TDD

--- chapter-07/todo/lib/repositories/isar/isar_board_repository.dart
+++ chapter-07/todo/lib/repositories/isar/isar_board_repository.dart
@@ -1,5 +1,3 @@
-import 'package:isar/isar.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:todo/models/task.dart';
 import 'package:todo/repositories/board_repository.dart';
 import 'package:todo/repositories/isar/isar_datasource.dart';
@@ -14,11 +12,37 @@ class IsarBoardRepository implements BoardRepository {
 
   @override
   Future<List<Task>> fetch() async {
-    throw UnimplementedError();
+    final models = await _datasource.getTasks();
+    return models
+        .map(
+          (e) => Task(
+            id: e.id,
+            description: e.description,
+            isChecked: e.isChecked,
+          ),
+        )
+        .toList();
   }
 
   @override
   Future<List<Task>> update({required List<Task> tasks}) async {
-    throw UnimplementedError();
+    final models = tasks.map((e) {
+      final model = TaskModel()
+        ..isChecked = e.isChecked
+        ..description = e.description;
+
+      if (e.id != -1) {
+        model.id = e.id;
+      }
+
+      return model;
+    }).toList();
+
+    await Future.wait([
+      _datasource.deleteAllTasks(),
+      _datasource.putAllTasks(models),
+    ]);
+
+    return tasks;
   }
 }
